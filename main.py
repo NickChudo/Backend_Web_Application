aff0f29235590b7b042266cba8140bd0976eb25f
kolya <kolyan.chudinovskix@mail.ru>
2024-11-07 20:44:15 +0500

    Скелет бэка работает, есть функция, которая принимает на вход аудио и выдаёт расшифровку в виде текста

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 # This is a sample Python script.
-
+from modelinit import *
+import warnings
+warnings.filterwarnings("ignore")
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    model_file= 'model.pth'
+    pretrained_model = ModelInit(model_file, 'cpu')
 
+    pred = pretrained_model.predict("D:/new_disorder_speeches_over_2000/Speeches/39.1.wav")
+    print(pred)
 
+main()
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
